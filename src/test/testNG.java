4893ca536bda2f1157a76457c1da82d316ede4ec
VENKATA DURGA RAO <VENKATA DURGA RAO@LAPTOP-JTB9O7NL.mshome.net>
2021-09-05 19:59:24 +0530

    first commit

---
+++ src/test/testNG.java
@@ -0,0 +1,16 @@
+package test;
+
+	
+
+	import org.testng.annotations.Test;
+
+	public class testNG {
+	  @Test
+	  public void testngexamplee() {
+		  
+		  venkat.sonardemo.main("demo");
+		  System.out.println("test is called");
+	  }
+	
+
+}
---
+++ src/venkat/sonardemo.java
@@ -0,0 +1,18 @@
+package venkat;
+
+public class sonardemo {
+	static int a=10;
+	static int b=20;
+	public static void main(String string) {
+		System.out.println("Welcome to SonarQube");
+		if(a>b) {
+			System.out.println("yes");
+		}
+		else
+			{
+				System.out.println("no");
+			}
+		
+	}
+
+}
